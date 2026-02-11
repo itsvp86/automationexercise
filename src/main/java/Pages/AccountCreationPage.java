6d3a5dbc86fc501365a96cd2328f526ae6b70448
vyankatesh panchal <vyankateshp86@gmail.com>
2026-02-11 17:20:34 +0530

    Clean up blank lines in AccountCreationPage.java
    Removed unnecessary blank lines in AccountCreationPage.java

--- src/main/java/Pages/AccountCreationPage.java
+++ src/main/java/Pages/AccountCreationPage.java
@@ -9,9 +9,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-
-
-
 public class AccountCreationPage 
 {
 	private WebDriver driver;
